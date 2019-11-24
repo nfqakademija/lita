ced793e8572e169419e166f07ff78231875c7f6e
dimacellist <dyanovyanovsky@gmail.com>
2019-11-24 23:40:36 +0200

    Fixed conflict between original AppFixtures.php file, returned deleted comments in AcademyRepository.php file

--- src/Repository/AcademyRepository.php
+++ src/Repository/AcademyRepository.php
@@ -19,6 +19,23 @@ class AcademyRepository extends ServiceEntityRepository
         parent::__construct($registry, Academy::class);
     }
 
+    // /**
+    //  * @return Academy[] Returns an array of Academy objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('a')
+            ->andWhere('a.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('a.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
     /*
     public function findOneBySomeField($value): ?Academy
     {
