04ee3a39591e75cc7583b4d59b0978bc75c99d8e
dimacellist <dyanovyanovsky@gmail.com>
2019-11-24 23:40:36 +0200

    Fixed conflict between original AppFixtures.php file, returned deleted comments in AcademyRepository.php file

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,11 +12,10 @@ class AppFixtures extends Fixture
     {
         for ($i = 0; $i < 8; $i++) {
             $academy = new Academy();
-
-            $academy->setAcademyName('Super Academy '.$i);
-            $academy->setAcademyEmail('academy@email.com');
-            $academy->setAcademyUrl('www.academy.com');
-            $academy->setAcademyLogo('www.google.com/image.jpg');
+            $academy->setAcademyName('Super Academy');
+            $academy->setAcademyEmail('info@nfq.com');
+            $academy->setAcademyUrl('https://www.nfq.lt/apie-akademija');
+            $academy->setAcademyLogo('https://e-komercija.eu/wp-content/uploads/2018/03/nfq-logo.jpg');
             $academy->setAcademyDescription('Some awesome description');
             $manager->persist($academy);
         }
