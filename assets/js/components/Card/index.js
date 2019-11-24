b88a5c3f5f51324b7faaa1280a6f7e3dd7246854
Kristina Rudzinskaitė <rudzinskaitekristina@gmail.com>
2019-11-24 16:42:46 +0200

    Translate from En to Lt

--- assets/js/components/Card/index.js
+++ assets/js/components/Card/index.js
@@ -8,32 +8,32 @@ class Card extends PureComponent {
         return (
             <div className="card promoting-card flex-wrap flex-row academy-card academy-card--shadow">
                     <div className="col-lg-8 card-body d-flex flex-row p-3">
-                        <img src={academy.academy_logo} className="logo rounded-circle mr-3" alt="..." />
+                        <img src={academy.academy_logo} className="logo rounded-circle mr-3" alt={academy.academy_name} />
                         <div>
                             <h5 className="card-title font-weight-bold mb-2">{academy.academy_name}</h5>
                             <p className="card-text"><i className="fa fa-calendar pr-2" aria-hidden="true" />2019-11-14</p>
-                            <p className="card-text">{academy.description ? academy.description : 'Super cool academy, with tons of homeworks'}</p>
+                            <p className="card-text">{academy.description ? academy.description : 'Labai puiki akademija, turi daug įvairių programų'}</p>
                         </div>
 
 
                     </div>
                     <div className="col-lg-4 p-3 d-flex flex-column justify-content-between card-right">
                         <div className="row">
-                            <div className="col">
-                                <i className="fa fa-eur pr-1" aria-hidden="true"></i><span className="price">{academy.price ? academy.price : 'Free'}</span>
+                            <div className="col-5">
+                                <i className="fa fa-eur pr-1" aria-hidden="true" /><span className="price">{academy.price ? academy.price : 'Nemokama'}</span>
                             </div>
-                            <div className="col">
-                                <i className="fa fa-clock-o pr-1" aria-hidden="true" />3 months
+                            <div className="col-4">
+                                <i className="fa fa-clock-o pr-1" aria-hidden="true" />3 mėn
                             </div>
-                            <div className="col">
+                            <div className="col-3">
                                 <i className="fa fa-map-marker  pr-1" aria-hidden="true" />Vilnius
                             </div>
                         </div>
                         <div className="d-flex flex-column align-items-end">
-                            <div>
-                                <a href="mailto:test@test.com" className="card-email"><i className="fa fa-envelope-o pr-1" aria-hidden="true" />{academy.academy_email}</a>
+                            <div className="mt-4 mb-5">
+                                <a href={academy.academy_url} className="card-link">{academy.academy_url}</a>
                             </div>
-                            <a href={academy.academy_url} className="btn btn-primary">Academy page link</a>
+                            <button className="btn btn-primary">Skaityti daugiau</button>
                         </div>
                 </div>
             </div>
