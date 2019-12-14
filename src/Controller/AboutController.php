d51b6491b8506a683ff522014a36e0d14a1e580b
Indre Rakstele <indruteok@gmail.com>
2019-12-14 14:00:32 +0200

    clean AboutController.php and HomeController.php

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutController extends AbstractController
 {
     /**
      * @Route("/about", name="about")
+     * @return Response
      */
     public function index()
     {
