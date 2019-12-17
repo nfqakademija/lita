c8a6475ef7bbffa1474b521d718aa22a8d79b2b1
dimacellist <dyanovyanovsky@gmail.com>
2019-12-17 13:54:10 +0200

    specific consumer can leave 1 review for specific program

--- src/Controller/ReviewsController.php
+++ src/Controller/ReviewsController.php
@@ -2,27 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Consumer;
 use App\Entity\Program;
 use App\Entity\Review;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReviewsController extends AbstractController
 {
     /**
-     * @Route("/api/v1/program/{id}/review",
+     * @Route("/api/v1/program/{program_id}/consumers/{googleId}/review",
      *     name="review",
      *     methods={"POST"},
-     *     requirements={"id"="\d+"})
-     * @param int $id
+     *     requirements={"program_id"="\d+", "googleId"="\d+"})
+     * @param int $program_id
+     * @param int $googleId
      * @param Request $request
      * @return JsonResponse
      * @throws Exception
      */
-    public function createReview(Request $request, $id): JsonResponse
+    public function createReview(Request $request, $program_id, $googleId): JsonResponse
     {
         $review = new Review();
 
@@ -32,16 +35,29 @@ class ReviewsController extends AbstractController
 
         $program = $this->getDoctrine()
             ->getRepository(Program::class)
-            ->find($id);
+            ->find($program_id);
 
-        $review->setReviewStars($review_stars);
-        $review->setReviewComment($review_comment);
-        $review->setReviewData($review_data);
-        $review->setProgram($program);
+        $user = $this->getDoctrine()
+            ->getRepository(Consumer::class)
+            ->find($googleId);
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($review);
-        $em->flush();
+        if ($user === null) {
+            return new JsonResponse("Please login with Google to leave a review", Response::HTTP_NOT_FOUND);
+        }
+
+        if ($user->getReviews()->count() == 0) {
+            $review->setReviewStars($review_stars);
+            $review->setReviewComment($review_comment);
+            $review->setReviewData($review_data);
+            $review->setProgram($program);
+            $review->setConsumer($user);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+        } else {
+            return new JsonResponse("Only One review is allowed per user", Response::HTTP_METHOD_NOT_ALLOWED);
+        }
 
         return new JsonResponse(
             'Review left successfully',
